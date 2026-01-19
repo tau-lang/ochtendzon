6b6ca4a9ee0f6c3b94a02450c2cebaf2c55bf3f4
Kiyotoko <karl.zschiebsch@gmail.com>
2026-01-19 19:45:35 +0100

    chore: split bin and manpages package

--- flake.nix
+++ flake.nix
@@ -1,40 +1,62 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-        tau.url = "github:tau-lang/tau";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    tau.url = "github:tau-lang/tau";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { nixpkgs, tau, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem(system:
-    let
-        pkgs = import nixpkgs {
-            inherit system;
-        };
+  outputs = { nixpkgs, tau, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = nixpkgs.lib;
         bi = [ tau.packages."${system}".default pkgs.gcc ];
-    in
-    {
-        packages.default = pkgs.stdenv.mkDerivation {
+      in {
+        packages = rec {
+          default = ochtendzon;
+
+          ochtendzon = pkgs.stdenv.mkDerivation {
             pname = "ochtendzon";
             version = "0.1.0";
 
             src = ./.;
 
-            nativeBuildInputs = [
-                pkgs.makeWrapper
-            ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
 
             installPhase = ''
-            mkdir -p $out/bin $out/share/man/man1
-            cp ochtendzon $out/bin/
-            cp man/ochtendzon.1 $out/share/man/man1/
+              mkdir -p $out/bin
+              cp ochtendzon $out/bin/
 
-            wrapProgram $out/bin/ochtendzon \
-            --prefix PATH : ${pkgs.lib.makeBinPath bi}
+              wrapProgram $out/bin/ochtendzon \
+              --prefix PATH : ${pkgs.lib.makeBinPath bi}
             '';
+
+            meta = {
+              homepage = "https://github.com/tau-lang/ochtendzon";
+              license = lib.licenses.eupl12;
+              platforms = lib.platforms.all;
+            };
+          };
+
+          ochtendzon-manpages = pkgs.stdenv.mkDerivation {
+            pname = "ochtendzon-manpages";
+            version = "0.1.0";
+
+            src = ./.;
+
+            installPhase = ''
+              mkdir -p $out/share/man/man1
+              cp man/ochtendzon.1 $out/share/man/man1/
+            '';
+
+            meta = {
+              homepage = "https://github.com/tau-lang/ochtendzon";
+              license = lib.licenses.eupl12;
+              platforms = lib.platforms.all;
+            };
+          };
         };
-        devShells.default = pkgs.mkShell {
-            buildInputs = bi;
-        };
-    });
+        devShells.default =
+          pkgs.mkShell { buildInputs = bi ++ [ pkgs.nixfmt ]; };
+      });
 }
